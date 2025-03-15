59e8d9c907a7f3ca3ce7f4b6543002e2c606789f
Clay ChanWoo <claychanwoo@naver.com>
2025-03-16 00:59:16 +0900

    refactor: head link 코드 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 
-import { Footer, Navbar } from '@/shared/index';
+import { Footer, HeadLinks, Navbar } from '@/shared/index';
 import '@/shared/styles/globals.css';
 import '@/shared/styles/reset.css';
 import ProgressBar from '@/shared/ui/ProgressBar';
@@ -42,30 +42,11 @@ export default function RootLayout({
 }>) {
   return (
     <html className={inter.variable} lang="ko">
-      <link href="/apple-icon-57x57.png" rel="apple-touch-icon" sizes="57x57" />
-      <link href="/apple-icon-60x60.png" rel="apple-touch-icon" sizes="60x60" />
-      <link href="/apple-icon-72x72.png" rel="apple-touch-icon" sizes="72x72" />
-      <link href="/apple-icon-76x76.png" rel="apple-touch-icon" sizes="76x76" />
-      <link href="/apple-icon-114x114.png" rel="apple-touch-icon" sizes="114x114" />
-      <link href="/apple-icon-120x120.png" rel="apple-touch-icon" sizes="120x120" />
-      <link href="/apple-icon-144x144.png" rel="apple-touch-icon" sizes="144x144" />
-      <link href="/apple-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
-      <link href="/apple-icon-180x180.png" rel="apple-touch-icon" sizes="180x180" />
-      <link href="/android-icon-192x192.png" rel="icon" sizes="192x192" type="image/png" />
-      <link href="/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
-      <link href="/favicon-96x96.png" rel="icon" sizes="96x96" type="image/png" />
-      <link href="/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-      <link href="/manifest.json" rel="manifest" />
-      <meta content="#ffffff" name="msapplication-TileColor" />
-      <meta content="/ms-icon-144x144.png" name="msapplication-TileImage" />
-      <meta content="#ffffff" name="theme-color" />
+      <HeadLinks />
       <body className={`font-sans antialiased`}>
         <ProgressBar />
         <Navbar />
         <main className="container mx-auto flex min-h-screen flex-1 flex-col bg-background px-4 py-8">{children}</main>
-        {/* <main className="flex min-h-screen flex-col bg-background">
-          <main className="container mx-auto flex-1 px-4 py-8">{children}</main>
-        </main> */}
         <Footer />
       </body>
     </html>
--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,4 +1,5 @@
 export * from './ui/Footer';
+export * from './ui/HeadLinks';
 export * from './ui/Navbar';
 export * from './ui/ProgressBar';
 export * from './utils/posts';
